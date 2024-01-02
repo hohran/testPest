bb766d974e8e0b3fcc277c6744f6b3d49da9cee3
Jan Hranička <hranickajan@seznam.cz>
2024-01-02 13:43:04 +0100

    Revert "remove second argument"
    This reverts commit 92d75c8ff1595e2e137d57cc6f57da43cdeee08f.

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		fprintf(stderr, "Honza je debil a zadej jeden argument programu.\n");
+	if (argc != 3) {
+		fprintf(stderr, "Honza je debil a zadej dvakrat argument programu.\n");
 		return 1;
 	}
 	printf("Hello %s!\n", argv[1]);
+	printf("You are %s.\n", argv[2]);
 }
