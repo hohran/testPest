f87606be0bd84d2cb3793d35970f88d4ef48d5ea
Jan Hranička <hranickajan@seznam.cz>
2024-01-02 13:40:20 +0100

    print name from program argument

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello World!\n");
+int main(int argc, char** argv) {
+	if (argc != 3) {
+		fprintf(stderr, "Honza je debil a zadej dvakrat argument programu.\n");
+		return 1;
+	}
+	printf("Hello %s!\n", argv[1]);
+	printf("You are %s.\n", argv[2]);
 }
