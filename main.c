92d75c8ff1595e2e137d57cc6f57da43cdeee08f
Jan Hranička <hranickajan@seznam.cz>
2024-01-02 13:41:12 +0100

    remove second argument

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-		fprintf(stderr, "Honza je debil a zadej dvakrat argument programu.\n");
+	if (argc != 2) {
+		fprintf(stderr, "Honza je debil a zadej jeden argument programu.\n");
 		return 1;
 	}
 	printf("Hello %s!\n", argv[1]);
-	printf("You are %s.\n", argv[2]);
 }
